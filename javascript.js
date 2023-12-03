0209fab76ec2472e3a37f2932ceec3fe61dc8203
Esmeralda Torres Aguirre <esmeraldatorresaguirre@MacBook-Air-de-Esmeralda.local>
2023-12-03 14:42:20 -0600

    change count down

--- javascript.js
+++ javascript.js
@@ -35,7 +35,7 @@ playPause.addEventListener("click", () => {
 
 // Cuenta regresiva -------------------------------------------------------------------
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 29, 2023 18:00").getTime();
+var countDownDate = new Date("Dec 23, 2023 14:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
